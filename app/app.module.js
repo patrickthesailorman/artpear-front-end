5fe4968e7d89d2ca0f64e16ef1e585a887281b63
patrickthesailorman <patrickthesaiorman@gmail.com>
2017-01-26 15:28:46 -0500

    add opportunities and seekers

--- app/app.module.js
+++ app/app.module.js
@@ -5,6 +5,9 @@
       'ui.router',
       'artists',
       'responses'
+      'artists'
+      // 'seekers'
+      'opportunities'
     ])
     .constant('API_URL', 'https://artpear-api.herokuapp.com/api/');
 
