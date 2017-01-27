a1c31d56d1b58888a027a2c68b70f674384f8204
patrickthesailorman <patrickthesaiorman@gmail.com>
2017-01-26 23:45:29 -0500

    fixed route to create opportunity

--- app/components/opportunities/opportunities.routes.js
+++ app/components/opportunities/opportunities.routes.js
@@ -12,7 +12,7 @@
         })
         .state('opportunitiesCreate', {
           url: '/opportunities/create',
-          templateUrl : 'app/components/opportunities/opportunityCreate.html',
+          templateUrl : 'app/components/opportunities/opportunitiesCreate.html',
           controller  : 'opportunitiesCreateCtrl'
         })
         .state('opportunitiesShow', {
